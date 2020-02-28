c68276b783142742deefcc62d691207d4adc7927
vinicius-araujo <vinisergio@gmail.com>
2020-02-28 16:49:09 -0300

    Set up initial strucuture

---
+++ src/app/_root-store/root-store.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GithubSearchUserModule } from './github-search-user/github-search-user.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GithubSearchUserModule
+  ]
+})
+export class RootStoreModule { }
